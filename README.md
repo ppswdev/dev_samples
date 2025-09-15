77259e8d94ff26f7823da80749e0e28ee37fbda2
xiaopin <251089096@qq.com>
2025-09-15 23:15:25 +0800

    my_vue更新

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # dev_samples
+
 开发学习示例
+
+## 项目清理
+
+``` json
+"scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview",
+    "lint": "eslint . --fix",
+    "format": "prettier --write src/",
+    "clean": "rm -rf node_modules pnpm-lock.yaml && pnpm store prune && pnpm install"
+  },
+
+```
+
+运行命令：`pnpm run clean`
--- web/my_vue/src/App.vue
+++ web/my_vue/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-import DemoPage from './components/DemoPage.vue'
+import HeroSection from './components/HeroSection.vue';
+import FeatureSecion from './components/FeatureSecion.vue';
+import Stats from './components/Stats.vue';
+import Footer from './components/Footer.vue';
+import DemoPage from './components/DemoPage.vue';
 </script>
 
 <template>
-  <DemoPage />
+  <DemoPage/>
+  <!-- <HeroSection />
+  <FeatureSecion />
+  <Stats />
+  <Footer /> -->
 </template>
 
 <style scoped></style>
